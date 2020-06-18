6c0f1f8b5e62158df0a10486502d766bc23ddb97
nate <nathantrinh23@yahoo.com>
2020-06-18 12:20:53 -0700

    Added embed output, alias, and type.
    Embed color comes from the [p]set colour command of Red. Alias is [p]ev. There is a problem with the type where it  says the type and the output without the first letter too. Not sure why that's happening.

--- eval/eval.py
+++ eval/eval.py
@@ -2,13 +2,17 @@ import js2py
 import discord
 from redbot.core import commands, checks, Config
 
-
 class eval(commands.Cog):
-    """eval boiiiii"""
-	
-    @commands.command()
+    """Evals code in JS."""
+
+    @commands.command(aliases=['ev'])
     @checks.is_owner()
     async def eval(self, ctx, *, eArg):
         eval = js2py.eval_js(eArg)
-        await ctx.send(eval)
-
+        type = js2py.eval_js('typeof '+eArg)
+        embed = discord.Embed(title="Eval", color=await ctx.embed_color())
+        await ctx.embed_color()
+        embed.add_field(name="Output", value=eval, inline=False)
+        embed.add_field(name="Type", value=type, inline=False)
+        embed.set_footer(text="Eval done by {}".format(ctx.message.author))
+        await ctx.send(embed=embed)
